ae51d45c3f227fc75f47475cf8b09622ac7522d8
PatrickSant0s <132529431+PatrickSant0s@users.noreply.github.com>
2025-04-02 21:45:04 -0300

    chore: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/NOME_DO_REPO/", // 🔥 Substitua pelo nome exato do seu repositório!
+  build: {
+    outDir: "dist",
+  },
+});
